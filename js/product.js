c3ce280395a9782a667b5f5a29ddd48e721ac27f
jonsnow2121 <46992154+jonsnow2121@users.noreply.github.com>
2020-04-02 15:20:00 +0530

    Add files via upload

---
+++ js/product.js
@@ -0,0 +1,78 @@
+
+var productData = [ { "imageCredit": { "artist": "Martin Wessely", "link": "http://www.resplashed.com/photographer/martin_wessely/" }, "tags": [ "Trees" ], "prodId":1, "imageUrl": "http://www.resplashed.com/img/400_002e1ecb8bd2.jpg", "filename": "400_002e1ecb8bd2.jpg", "imageHash": "2b1d5b5ef4b37b4f5d0dccade1b69987", "price": 10.99, "name": "pen 1", "description": "enim corporis voluptatibus laudantium possimus alias dolorem voluptatem similique aut aliquam voluptatem voluptatem omnis id consequatur", "slug": "Handcrafted-Trees-Mug", "added": 1485723766805, "manufacturer": "OHara-Group", "itemType": "pen", "itemCat": "stationary", "productImg": "mug-400_002e1ecb8bd2.jpg" }, { "imageCredit": { "artist": "Patrick Fore", "link": "http://www.resplashed.com/photographer/patrick_fore/" }, "tags": [ "Beach", "Ocean", "Water" ], "prodId":2,"imageUrl": "http://www.resplashed.com/img/400_00b425ea69e0.jpg", "filename": "400_00b425ea69e0.jpg", "imageHash": "9197d084a8309c5cc1c1860063bc4dda", "price": 19.99, "name": "pencil 2", "description": "totam at veritatis eligendi assumenda ex quia praesentium quibusdam ducimus", "slug": "Rustic-Beach-Mug", "added": 1481573896833, "manufacturer": "Sipes-Inc", "itemType": "pencil", "itemCat": "stationary", "productImg": "mug-400_00b425ea69e0.jpg" }, { "imageCredit": { "artist": "Adam Willoughby-Knox", "link": "http://www.resplashed.com/photographer/adam_willoughby-knox/" }, "tags": [ "Animal", "Bear" ], "prodId":3,"imageUrl": "http://www.resplashed.com/img/400_016a0ffdc46d.jpg", "filename": "400_016a0ffdc46d.jpg", "imageHash": "7389141c2b2657f5b2531cfd8c290759", "price": 17.99, "name": "Handcrafted Bear Mug", "description": "vitae mollitia et in accusantium est voluptas officiis est non", "slug": "Handcrafted-Bear-Mug", "added": 1485991071829, "manufacturer": "Weissnat-Schowalter-and-Koelpin", "itemType": "mug", "itemCat": "utensils", "productImg": "mug-400_016a0ffdc46d.jpg" }, { "imageCredit": { "artist": "Eric Huang", "link": "http://www.resplashed.com/photographer/eric_huang/" }, "tags": [ "Road" ], "prodId":4,"imageUrl": "http://www.resplashed.com/img/400_01828ff91dc5.jpg", "filename": "400_01828ff91dc5.jpg", "imageHash": "acf59094c5fe008880e72aa4a16bd6ee", "price": 15.99, "name": "Refined Road Mug", "description": "explicabo fugit magnam fugit dolorem itaque unde quidem est quia ut a veritatis sit facere possimus fugit ipsam", "slug": "Refined-Road-Mug", "added": 1482213983048, "manufacturer": "Bernier-Hane", "itemType": "mug", "itemCat": "utensils", "productImg": "mug-400_01828ff91dc5.jpg" }, { "imageCredit": { "artist": "Olivia Henry", "link": "http://www.resplashed.com/photographer/olivia_henry/" }, "tags": [ "Ocean", "Rocks" ], "prodId":5,"imageUrl": "http://www.resplashed.com/img/400_018cf84f5121.jpg", "filename": "400_018cf84f5121.jpg", "imageHash": "2967736ddb9ae0fdf18563622fccb2cc", "price": 10.99, "name": "Unbranded Ocean Mug", "description": "facilis aut velit vitae sit dolorum illum nostrum pariatur dolorem vel atque quasi placeat qui", "slug": "Unbranded-Ocean-Mug", "added": 1483408192004, "manufacturer": "Franecki---Gaylord", "itemType": "earaser", "itemCat": "stationary", "productImg": "mug-400_018cf84f5121.jpg" }, { "imageCredit": { "artist": "Zwaddi", "link": "http://www.resplashed.com/photographer/zwaddi/" }, "tags": [ "Sunset", "Beach", "Ocean" ], "prodId":6,"imageUrl": "http://www.resplashed.com/img/400_01ca9c693d2c.jpg", "filename": "400_01ca9c693d2c.jpg", "imageHash": "0753f3afeee4eda16d0b6e5775ee2b29", "price": 9.99, "name": "Rustic Ocean Mug", "description": "libero reprehenderit blanditiis quidem laborum ullam quae fuga consequuntur minima praesentium consequatur qui excepturi nostrum tempora sunt deleniti", "slug": "Rustic-Ocean-Mug", "added": 1485511118573, "manufacturer": "Bayer-and-Sons", "itemType": "glass", "itemCat": "utensils", "productImg": "mug-400_01ca9c693d2c.jpg" }, { "imageCredit": { "artist": "jamie mink", "link": "http://www.resplashed.com/photographer/jamie_mink/" }, "tags": [ "Rust", "Old", "Car" ], "prodId":7,"imageUrl": "http://www.resplashed.com/img/400_01d403b11a9b.jpg", "filename": "400_01d403b11a9b.jpg", "imageHash": "40dd32ff718ba70b8b8a1850e530d2e2", "price": 19.99, "name": "pen 3", "description": "necessitatibus suscipit iure quia voluptates voluptas quidem ipsum laboriosam adipisci", "slug": "Sleek-Old-Mug", "added": 1485759837725, "manufacturer": "Boyle-LLC", "itemType": "pen", "itemCat": "stationary", "productImg": "mug-400_01d403b11a9b.jpg" }, { "imageCredit": { "artist": "Justin Leibow", "link": "http://www.resplashed.com/photographer/justin_leibow/" }, "tags": [ "Coffee" ], "prodId":8,"imageUrl": "http://www.resplashed.com/img/400_01fdff8681fc.jpg", "filename": "400_01fdff8681fc.jpg", "imageHash": "abc437b613c34ab96391291112b6187b", "price": 14.99, "name": "Sleek Coffee Mug 2", "description": "et adipisci explicabo consequatur rerum voluptas dolorem qui omnis vel", "slug": "Sleek-Coffee-Mug", "added": 1479259996312, "manufacturer": "Franecki---Gaylord", "itemType": "mug", "itemCat": "utensils", "productImg": "mug-400_01fdff8681fc.jpg" }, { "imageCredit": { "artist": "Stefania Bonacasa", "link": "http://www.resplashed.com/photographer/stefania_bonacasa/" }, "tags": [ "Trees", "Fog", "People" ], "prodId":9,"imageUrl": "http://www.resplashed.com/img/400_026bcd988f43.jpg", "filename": "400_026bcd988f43.jpg", "imageHash": "119b67f433042dd6ee733e4cb4096077", "price": 16.99, "name": "Rustic Trees Mug 3", "description": "ut architecto est similique sit nostrum sit sed ipsa itaque aliquam nesciunt reprehenderit et neque aut id nulla dolore sed sit facere eligendi", "slug": "Rustic-Trees-Mug", "added": 1480664800826, "manufacturer": "Weissnat-Schowalter-and-Koelpin", "itemType": "mug", "itemCat": "utensils", "productImg": "mug-400_026bcd988f43.jpg" } ];
+var prodCat = ['stationary','utensils'];
+
+
+var jsonResponse = {};
+jsonResponse.productData = productData;
+jsonResponse.prodCat = prodCat;
+var cartArray=[];
+localStorage.setItem("jsonResponse",JSON.stringify(jsonResponse));
+localStorage.setItem("cartArray",JSON.stringify(cartArray));
+var itemsInCart = JSON.parse(localStorage.cartArray).length;
+function showProds(){
+	var productData = [ { "imageCredit": { "artist": "Martin Wessely", "link": "http://www.resplashed.com/photographer/martin_wessely/" }, "tags": [ "Trees" ], "prodId":1, "imageUrl": "http://www.resplashed.com/img/400_002e1ecb8bd2.jpg", "filename": "400_002e1ecb8bd2.jpg", "imageHash": "2b1d5b5ef4b37b4f5d0dccade1b69987", "price": 10.99, "name": "pen 1", "description": "enim corporis voluptatibus laudantium possimus alias dolorem voluptatem similique aut aliquam voluptatem voluptatem omnis id consequatur", "slug": "Handcrafted-Trees-Mug", "added": 1485723766805, "manufacturer": "OHara-Group", "itemType": "pen", "itemCat": "stationary", "productImg": "mug-400_002e1ecb8bd2.jpg" }, { "imageCredit": { "artist": "Patrick Fore", "link": "http://www.resplashed.com/photographer/patrick_fore/" }, "tags": [ "Beach", "Ocean", "Water" ], "prodId":2,"imageUrl": "http://www.resplashed.com/img/400_00b425ea69e0.jpg", "filename": "400_00b425ea69e0.jpg", "imageHash": "9197d084a8309c5cc1c1860063bc4dda", "price": 19.99, "name": "pencil 2", "description": "totam at veritatis eligendi assumenda ex quia praesentium quibusdam ducimus", "slug": "Rustic-Beach-Mug", "added": 1481573896833, "manufacturer": "Sipes-Inc", "itemType": "pencil", "itemCat": "stationary", "productImg": "mug-400_00b425ea69e0.jpg" }, { "imageCredit": { "artist": "Adam Willoughby-Knox", "link": "http://www.resplashed.com/photographer/adam_willoughby-knox/" }, "tags": [ "Animal", "Bear" ], "prodId":3,"imageUrl": "http://www.resplashed.com/img/400_016a0ffdc46d.jpg", "filename": "400_016a0ffdc46d.jpg", "imageHash": "7389141c2b2657f5b2531cfd8c290759", "price": 17.99, "name": "Handcrafted Bear Mug", "description": "vitae mollitia et in accusantium est voluptas officiis est non", "slug": "Handcrafted-Bear-Mug", "added": 1485991071829, "manufacturer": "Weissnat-Schowalter-and-Koelpin", "itemType": "mug", "itemCat": "utensils", "productImg": "mug-400_016a0ffdc46d.jpg" }, { "imageCredit": { "artist": "Eric Huang", "link": "http://www.resplashed.com/photographer/eric_huang/" }, "tags": [ "Road" ], "prodId":4,"imageUrl": "http://www.resplashed.com/img/400_01828ff91dc5.jpg", "filename": "400_01828ff91dc5.jpg", "imageHash": "acf59094c5fe008880e72aa4a16bd6ee", "price": 15.99, "name": "Refined Road Mug", "description": "explicabo fugit magnam fugit dolorem itaque unde quidem est quia ut a veritatis sit facere possimus fugit ipsam", "slug": "Refined-Road-Mug", "added": 1482213983048, "manufacturer": "Bernier-Hane", "itemType": "mug", "itemCat": "utensils", "productImg": "mug-400_01828ff91dc5.jpg" }, { "imageCredit": { "artist": "Olivia Henry", "link": "http://www.resplashed.com/photographer/olivia_henry/" }, "tags": [ "Ocean", "Rocks" ], "prodId":5,"imageUrl": "http://www.resplashed.com/img/400_018cf84f5121.jpg", "filename": "400_018cf84f5121.jpg", "imageHash": "2967736ddb9ae0fdf18563622fccb2cc", "price": 10.99, "name": "Unbranded Ocean Mug", "description": "facilis aut velit vitae sit dolorum illum nostrum pariatur dolorem vel atque quasi placeat qui", "slug": "Unbranded-Ocean-Mug", "added": 1483408192004, "manufacturer": "Franecki---Gaylord", "itemType": "earaser", "itemCat": "stationary", "productImg": "mug-400_018cf84f5121.jpg" }, { "imageCredit": { "artist": "Zwaddi", "link": "http://www.resplashed.com/photographer/zwaddi/" }, "tags": [ "Sunset", "Beach", "Ocean" ], "prodId":6,"imageUrl": "http://www.resplashed.com/img/400_01ca9c693d2c.jpg", "filename": "400_01ca9c693d2c.jpg", "imageHash": "0753f3afeee4eda16d0b6e5775ee2b29", "price": 9.99, "name": "Rustic Ocean Mug", "description": "libero reprehenderit blanditiis quidem laborum ullam quae fuga consequuntur minima praesentium consequatur qui excepturi nostrum tempora sunt deleniti", "slug": "Rustic-Ocean-Mug", "added": 1485511118573, "manufacturer": "Bayer-and-Sons", "itemType": "glass", "itemCat": "utensils", "productImg": "mug-400_01ca9c693d2c.jpg" }, { "imageCredit": { "artist": "jamie mink", "link": "http://www.resplashed.com/photographer/jamie_mink/" }, "tags": [ "Rust", "Old", "Car" ], "prodId":7,"imageUrl": "http://www.resplashed.com/img/400_01d403b11a9b.jpg", "filename": "400_01d403b11a9b.jpg", "imageHash": "40dd32ff718ba70b8b8a1850e530d2e2", "price": 19.99, "name": "pen 3", "description": "necessitatibus suscipit iure quia voluptates voluptas quidem ipsum laboriosam adipisci", "slug": "Sleek-Old-Mug", "added": 1485759837725, "manufacturer": "Boyle-LLC", "itemType": "pen", "itemCat": "stationary", "productImg": "mug-400_01d403b11a9b.jpg" }, { "imageCredit": { "artist": "Justin Leibow", "link": "http://www.resplashed.com/photographer/justin_leibow/" }, "tags": [ "Coffee" ], "prodId":8,"imageUrl": "http://www.resplashed.com/img/400_01fdff8681fc.jpg", "filename": "400_01fdff8681fc.jpg", "imageHash": "abc437b613c34ab96391291112b6187b", "price": 14.99, "name": "Sleek Coffee Mug 2", "description": "et adipisci explicabo consequatur rerum voluptas dolorem qui omnis vel", "slug": "Sleek-Coffee-Mug", "added": 1479259996312, "manufacturer": "Franecki---Gaylord", "itemType": "mug", "itemCat": "utensils", "productImg": "mug-400_01fdff8681fc.jpg" }, { "imageCredit": { "artist": "Stefania Bonacasa", "link": "http://www.resplashed.com/photographer/stefania_bonacasa/" }, "tags": [ "Trees", "Fog", "People" ], "prodId":9,"imageUrl": "http://www.resplashed.com/img/400_026bcd988f43.jpg", "filename": "400_026bcd988f43.jpg", "imageHash": "119b67f433042dd6ee733e4cb4096077", "price": 16.99, "name": "Rustic Trees Mug 3", "description": "ut architecto est similique sit nostrum sit sed ipsa itaque aliquam nesciunt reprehenderit et neque aut id nulla dolore sed sit facere eligendi", "slug": "Rustic-Trees-Mug", "added": 1480664800826, "manufacturer": "Weissnat-Schowalter-and-Koelpin", "itemType": "mug", "itemCat": "utensils", "productImg": "mug-400_026bcd988f43.jpg" } ];
+var prodCat = ['stationary','utensils'];
+
+var jsonResponse = {};
+jsonResponse.productData = productData;
+jsonResponse.prodCat = prodCat;
+jsonResponse.isFilter = undefined;
+$('#viewProductDiv').html('');
+$('#viewProductDiv').setTemplate($('#vPTemplate').val());
+$('#viewProductDiv').processTemplate(jsonResponse);
+filterCat();
+addToCart();
+}
+
+function filterCat(){
+	$("#catSel").unbind("change").change(function() {
+				let  selectedCat = $(this).val();
+				let jsonResponse = JSON.parse(localStorage.getItem('jsonResponse'));
+				let data = jsonResponse.productData;
+				let dataLn = jsonResponse.productData.length;
+				let prdata = [];
+				if(selectedCat != 0){
+					for(let i=0;i<dataLn;i++){
+						if(data[i].itemCat == selectedCat){
+							prdata.push(data[i]);
+						}
+					}
+					jsonResponse.isFilter = 1;
+					jsonResponse.productData = prdata;
+					$('#viewProductDiv').html('');
+					$('#viewProductDiv').setTemplate($('#vPTemplate').val());
+					$('#viewProductDiv').processTemplate(jsonResponse);
+					addToCart();	
+					$('.clearfilter').off('click').on('click',function(){
+						showProds();
+				
+			});
+				}
+			});
+			
+		
+}
+showProds();
+function addToCart(){
+	$('.addToCart').off('click').on('click',function(){
+		let prodId = $(this).attr('prodId');
+		let cartObj = {};
+		let cartArray = JSON.parse(localStorage.cartArray);
+		let jsonResponse = JSON.parse(localStorage.getItem('jsonResponse'));
+		let data = jsonResponse.productData;
+		let dataLn = jsonResponse.productData.length;
+					for(let i=0;i<dataLn;i++){
+						if(data[i].prodId == prodId){
+							cartObj = data[i];
+							cartArray.push(cartObj);
+							localStorage.setItem('cartArray',JSON.stringify(cartArray));
+							itemsInCart = JSON.parse(localStorage.cartArray).length
+							$('#cartCount').html(itemsInCart);
+							alert("Product added to cart, Please click on cart tab to see your added products");
+						}
+					}
+		
+	});
+	
+}
